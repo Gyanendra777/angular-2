e3c55f874b5e5c5f824673f306adc08bbdeff770
GYANENDRA DUBEY <rajudubey1000@gmail.com>
2021-02-25 22:22:47 +0530

    df

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,22 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  //////////// पहला इंटरसेप्टर पहले अनुरोध को संभालता है////////
-  // httplnterceptor[0]
-  // पहला इंटरसेप्टर अंतिम पर प्रतिक्रिया को संभालता है
-  intercept (req,next){
-    return next.handle(req).pipe(
-      map(resp => resp)
-    );
-  }
-  // httplnterceptor[1]
-  intercept (req,next){
-    return next.handle(req).pipe(
-      map(resp => resp)
-    );
-  }
-    ///////////
-  
  
   constructor() {     
   } 
