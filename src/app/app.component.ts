b839c56f3afbfe89e7d267f9cd37139e41b5d177
GYANENDRA DUBEY <rajudubey1000@gmail.com>
2021-03-08 19:57:38 +0530

    output input passing data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,19 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  data=[
-  {name:'rsdfsaju',id:1},
-  {name:'sdfdfjkhgdj',id:2},
-  {name:'rajudsfkjhfs',id:3},
-  {name:'rajusdjf',id:4},
-  {name:'rajsdfkku',id:5},
-  ]
-  constructor() {     
+export class AppComponent { 
+  v:any="" ;
+  s
+  sentdata(raju){ 
+    this.s = raju
+  }
+  parentComponent($event){
+    this.v = $event;
+  }
+  constructor() {   
+   
   } 
 }
--- src/app/servi/serv222/serv222.component.ts
+++ src/app/servi/serv222/serv222.component.ts
@@ -1,18 +1,32 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit, Output,EventEmitter, Input } from '@angular/core';
 // import { ServiService } from 'src/app/servi.service';
 
 @Component({
   selector: 'app-serv222',
   templateUrl: './serv222.component.html',
-  styleUrls: ['./serv222.component.css']
+  styleUrls: ['./serv222.component.css'],
+  
 })
 export class Serv222Component implements OnInit {
+@Output()  childComponent:EventEmitter<any> = new EventEmitter()
 @Input() child
-  constructor() { }
 
+ 
+
+  constructor() {
+    // console.log(this.raje);
+   }
 
 
   ngOnInit(): void {
+    // this.childComponent.emit("{name:'raju', id:1},{name:'raju', id:1},{name:'raju', id:1},")
+
   }
+  sentdata(ra){
+    this.childComponent.emit(ra) 
+    
+  }
+  
+  
  
 }
